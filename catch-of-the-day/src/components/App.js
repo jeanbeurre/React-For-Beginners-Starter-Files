a3cac714e3016d28b38c86751624a692ea8546e4
Dominique Leger <Dominique.Leger@onespan.com>
2021-03-28 17:00:32 -0400

    Finished lesson 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
@@ -22,14 +24,40 @@ class App extends React.Component {
     console.log("Adding a fish!");
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    // 1. Take a copy of the existing state
+    const order = { ...this.state.order };
+    // 2. Add to the order or update the quantity in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Update the state object
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="I'm cool" age={500} cool={true} />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+                index={key}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
